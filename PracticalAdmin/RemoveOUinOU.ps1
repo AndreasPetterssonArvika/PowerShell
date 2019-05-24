7637ef9741de41d1d732124753504b4ec0da0dd8
Andreas Pettersson <andreas.pettersson@arvika.com>
2019-05-24 08:09:50 +0200

    Migration
    Renamed script for creating OU:s and added the reverse script for removing OU:s

---
+++ PracticalAdmin/RemoveOUinOU.ps1
@@ -0,0 +1,12 @@
+﻿# Removes a sub-OU from all OU:s in a LDAP path
+# The script handles OU:s protected from accidental deletion
+
+$baseOU = '<OU>'
+$targetOU = '<Name>'
+
+$computerOUs = Get-ADOrganizationalUnit -Filter * -SearchBase $FSKOU | Where-Object DistinguishedName -Like "OU=$targetOU,OU=*" | Select-Object -ExpandProperty DistinguishedName
+
+foreach ($computerOU in $computerOUs) {
+    Set-ADObject -Identity $computerOU -ProtectedFromAccidentalDeletion $false
+    Remove-ADOrganizationalUnit -Identity $computerOU
+}
