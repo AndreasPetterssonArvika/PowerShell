22e60263b4694d47cbac0c0f92c1088affa3347d
Andreas Pettersson <andreas.pettersson@arvika.com>
2019-05-24 08:47:58 +0200

    Migration
    Changed names of a folder. Migrated another script

---
+++ ActiveDirectoryAdmin/ImportSessionWithADModule.ps1
@@ -0,0 +1,12 @@
+﻿# Import Active Directory module in a remote session and import the session to your local PowerShell
+
+$server = '<server>'
+
+$credential = Get-Credential
+$session = New-PSSession -Name $server -Credential $credential
+
+Invoke-Command -Session $session -ScriptBlock { Import-Module ActiveDirectory }
+Import-PSSession -Session $session -Module ActiveDirectory
+
+# Don't forget to remove the session afterwards!
+# Remove-PSSession -Session $session
