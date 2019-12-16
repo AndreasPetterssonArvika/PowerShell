cb1c87296d5606a551e65150e15a4b202b2d39a2
Andreas Pettersson <andreas.pettersson@arvika.com>
2019-12-16 11:55:01 +0100

    Create Delete-UserFiles.ps1
    Simple script to delete files from the common user folders

---
+++ ClientAdmin/Delete-UserFiles.ps1
@@ -0,0 +1,7 @@
+﻿# Clears out user folders
+#Remove-Item "$env:USERPROFILE\Documents\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Downloads\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Desktop\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Favorites\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Videos\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Music\*.*" -Recurse
