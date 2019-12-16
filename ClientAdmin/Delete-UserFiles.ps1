14725b393c32ae47aae05cfffd564fa0a92dfada
Andreas Pettersson <andreas.pettersson@arvika.com>
2019-12-16 11:58:46 +0100

    Update Delete-UserFiles.ps1
    Added Pictures folder and explanation of why all rows are commented out

--- ClientAdmin/Delete-UserFiles.ps1
+++ ClientAdmin/Delete-UserFiles.ps1
@@ -1,7 +1,9 @@
-﻿# Clears out user folders
+﻿# Clears out user folders, intended to be used on logout
+# All rows commented to lessen risk of disaster
 #Remove-Item "$env:USERPROFILE\Documents\*.*" -Recurse
 #Remove-Item "$env:USERPROFILE\Downloads\*.*" -Recurse
 #Remove-Item "$env:USERPROFILE\Desktop\*.*" -Recurse
 #Remove-Item "$env:USERPROFILE\Favorites\*.*" -Recurse
 #Remove-Item "$env:USERPROFILE\Videos\*.*" -Recurse
 #Remove-Item "$env:USERPROFILE\Music\*.*" -Recurse
+#Remove-Item "$env:USERPROFILE\Pictures\*.*" -Recurse
