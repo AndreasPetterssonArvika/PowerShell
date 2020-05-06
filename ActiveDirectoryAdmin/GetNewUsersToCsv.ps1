c4a37391536b2735c85bf251bcd189d0d3b57274
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2020-05-06 16:43:19 +0200

    Update GetNewUsersToCsv.ps1
    Sorted users by creation date

--- ActiveDirectoryAdmin/GetNewUsersToCsv.ps1
+++ ActiveDirectoryAdmin/GetNewUsersToCsv.ps1
@@ -3,11 +3,14 @@
 # - Users with title 'Elev' as they're students
 # - Users lacking data in the personNummer attribute
 # - Users lacking a valid email address
+#
+# Users are sorted by creation date
 
 $DateCutOff=(Get-Date).AddDays(-30)
 Get-ADUser -Filter * -Property givenName,SN,whenCreated,personNummer,mail,title | Where {$_.whenCreated -gt $datecutoff `
                                                                                 -and $_.title -ne 'Elev' `
                                                                                 -and $_.personNummer -match "[0-9][0-9][0-9][0-9][0-9][0-9]*" `
                                                                                 -and $_.mail -match "[A-Za-z0-9._%+-]+[A-Za-z0-9.-]+\.[A-Za-z]"} | `
-                                                                                Select-Object givenName,SN,Name,personNummer,mail | `
+                                                                                Select-Object givenName,SN,Name,personNummer,mail,whenCreated | `
+                                                                                Sort-Object whenCreated | `
                                                                                 Export-Csv -Path 'C:\temp\users.csv' -NoTypeInformation -Delimiter ';' -Encoding UTF8
