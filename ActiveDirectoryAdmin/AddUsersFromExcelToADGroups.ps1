ced1fd53eabe53e48db8ba4219539dd41d52cda0
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-08-13 15:50:04 +0200

    Create AddUsersFromExcelToADGroups.ps1
    Add users from Excel to AD groups

---
+++ ActiveDirectoryAdmin/AddUsersFromExcelToADGroups.ps1
@@ -0,0 +1,18 @@
+﻿# Requires the ImportExcel Module
+# Adds users to groups based on an Excel Workbook with users on worksheets.
+# Groups are named the same except the identifying number at the end.
+# The corresponding worksheets are identified byt the same number.
+
+$workbook = '<path to file>'
+$worksheets = @('70','71','72','80','81','82','90','91','92')
+
+ForEach ( $worksheet in $worksheets) {
+    $groupName = "GRMinnebergsskolanPersonal$worksheet"
+    $group = Get-ADGroup -Identity $groupName
+    #$group
+    $userList = Import-Excel -Path $workbook -WorksheetName $worksheet -StartColumn 1 -EndColumn 1 -NoHeader
+    $mailAddresses = $userList.P1
+    #foreach ($mail in $mailAddresses) { $mail }
+    #foreach ($mail in $mailAddresses) { Get-ADUser -LDAPFilter "(mail=$mail)" }
+    foreach ($mail in $mailAddresses) { Get-ADUser -LDAPFilter "(mail=$mail)" | Add-ADPrincipalGroupMembership -MemberOf $group }
+}
