ff597a99f99352ed8df54c2680c4e4e58b68505d
Andreas Pettersson <andreas.pettersson@arvika.se>
2021-09-02 08:21:24 +0200

    Create DeleteOldFilesInSubfolders.ps1
    Simple script to delete old text files in folder with subfolders

---
+++ Utility/DeleteOldFilesInSubfolders.ps1
@@ -0,0 +1,13 @@
+﻿$path = "<path to root folder>"
+$daysOld = -30
+$timeLimit = (Get-Date).AddDays($daysOld)
+
+cd $path
+$files = Get-ChildItem -Recurse -Directory | ForEach-Object { $_.GetFiles()} | Where-Object { $_.Extension -eq ".txt" -and $_.CreationTime -lt $timeLimit}
+$files
+
+foreach ($file in $files) {
+    Write-host "Deleting file $file";
+    #Remove-Item $file.FullName -WhatIf
+    Remove-Item $file.FullName
+}
