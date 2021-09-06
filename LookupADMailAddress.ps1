fc64e0760fc6ab708eb25a94c0ebe0577027be15
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-09-06 10:53:27 +0200

    Look up mail address
    Script that looks up the mail address of a user.
    Note that the search of proxy addresses handles situations where there is additional protocol info

---
+++ LookupADMailAddress.ps1
@@ -0,0 +1,12 @@
+﻿# Slå upp mailadress i AD- attributen mail och proxyAddresses
+#
+# Sökningen i proxyAddresses har wildcards för att tillåta
+# formatering för olika tjänster
+
+$ADUser = '<username>'
+$ADDomain = '<maildomain>'
+$ldapfilter = "(|(mail=$ADUser@$ADDomain)(proxyaddresses=*$ADUser@$ADDomain*))"
+$ADattributes = @('mail','description')
+
+Get-ADUser -LDAPFilter $ldapfilter -Properties $ADattributes
+
