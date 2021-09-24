52d12838f2caba021c12611e5418e5d68be9ef1b
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-09-24 16:33:12 +0200

    Create FilePickerDialog.ps1
    Simple function that picks a file from the file system

---
+++ Examples/SingleSubjectExamples/FilePickerDialog.ps1
@@ -0,0 +1,13 @@
+﻿# Dialogruta för att välja fil
+
+Function Get-FileName($initialDirectory)
+{  
+    [System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”) | Out-Null
+
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = “Textfiler (*.txt)| *.txt”
+    $OpenFileDialog.Title = "Välj fil"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
