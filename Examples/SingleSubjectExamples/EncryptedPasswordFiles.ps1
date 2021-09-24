135ee74e70a4b37ee654eafc853b5dc4cf7580f6
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-09-24 16:24:48 +0200

    Create EncryptedPasswordFiles.ps1
    Short example demonstrating how to create encrypted password files

---
+++ Examples/SingleSubjectExamples/EncryptedPasswordFiles.ps1
@@ -0,0 +1,12 @@
+﻿# Skapa krypterade filer med lösenord för att slippa lagra lösenord i klartext.
+
+# Generera kryptonyckel till fil
+$key = New-Object Byte[] 32 ([Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($key)
+$key | Out-File <pathToKeyFile>
+
+# Kryptera ett lösenord till fil med en kryptonyckel på fil
+(Get-Credential).Password | ConvertFrom-SecureString -Key (Get-Content <pathToKeyFile>) | Set-Content <pathToPasswordFile>
+
+# Dekryptera lösenord från fil till Credential
+$password = Get-Content <pathToPasswordFile> | ConvertTo-SecureString -Key (Get-Content <pathToKeyFile>)
+$credential = New-Object System.Management.Automation.PSCredential(<username>,$password)
