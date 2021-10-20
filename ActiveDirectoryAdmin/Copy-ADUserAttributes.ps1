f001b0e7e14ef98a8c302c4c91a4fd2c9937a5ab
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-10-20 12:08:18 +0200

    Create Copy-ADUserAttributes.ps1
    Initial effort, getting attributes from ADUser

---
+++ ActiveDirectoryAdmin/Copy-ADUserAttributes.ps1
@@ -0,0 +1,19 @@
+﻿# Script copies attributes from one user to another
+#
+# Parameters:
+# - From-user, the user to be copied from
+# - To-user, the user to be copied to
+# - AttributesToCopy, the attributes to be copied
+
+$attributes = @('description','title','department','company','manager','streetAddress','l','telephoneNumber','physicalDeliveryOfficeName')
+
+(Get-ADUser pelle.testsson -Properties $attributes).PSObject.Properties | foreach { $hash=@{} } {$hash.add($_.Name, $_.value) }
+
+$hash
+
+$hash.Keys
+
+foreach ( $key in $attributes ) {
+    $tempval = $hash.$key
+    "$key is $tempval"
+}
