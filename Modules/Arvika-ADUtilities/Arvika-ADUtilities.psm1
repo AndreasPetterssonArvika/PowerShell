674aecd847c4e60a016f333f15c2690b84e0fc3e
Andreas Pettersson <andreas.pettersson@arvika.se>
2021-10-22 15:23:31 +0200

    Starting out

---
+++ Modules/Arvika-ADUtilities/Arvika-ADUtilities.psm1
@@ -0,0 +1,40 @@
+# Script copies attributes from one user to another
+#
+# Parameters:
+# - $copyFromUser, the user to be copied from
+# - $userToCopyTo, the user to be copied to, sAMAccountName
+# - $userAttributes, the attributes to be copied
+function Copy-ADUserAttributes {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        $copyToUser
+        $copyFromUser
+        $userAttributes
+    )
+
+    # Låt stå tills skriptet bättre testat
+    BREAK
+    # Låt stå tills skriptet bättre testat
+
+    $userToCopyFrom = '<sAMAccountName>'
+
+    $userToCopyTo = '<sAMAccountName>'
+
+    $attributes = @('description','title','department','company','manager','streetAddress','l','telephoneNumber','physicalDeliveryOfficeName')
+
+    (Get-ADUser $userToCopyFrom -Properties $attributes).PSObject.Properties | foreach { $copiedAttribs=@{} } {$copiedAttribs.add($_.Name, $_.value) }
+
+    $attribsToSet = @{}
+
+    foreach ( $attribute in $attributes ) {
+        if ( $copiedAttribs.$attribute) {
+            $attribsToSet.Add($attribute,$copiedAttribs.$attribute)
+        }
+    }
+
+    Set-ADUser $userToCopyTo -Replace $attribsToSet
+
+}
+
+
+
