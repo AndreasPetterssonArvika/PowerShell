40ca874c349a0778fb465feb5b67a168cbe475ae
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-08 15:55:05 +0200

    Added get-script for computer properties

---
+++ ClientAdmin/Get-ComputerPropertiesToCSVFile.ps1
@@ -0,0 +1,22 @@
+﻿<#
+
+Skriptet hämtar information om datorer
+
+Parametrar
+- ComputerNameFile, textfil med ett datornamn per rad
+- CSVResultFile, namn på utdatafilen. Om bara filnamn anges läggs data i samma mapp som skriptet
+- Properties, kommaseparerad lista med de properties man vill ha.
+
+#>
+
+param (
+    [string]$ComputerNameFile,
+    [string]$CSVResultFile,
+    [string[]]$Properties    # Notera skillnaden i datatyp, den här parametern förväntas vara en array
+)
+
+# Läs in datornamnen till en variabel
+$computerNames = Get-Content $ComputerNameFile
+
+# Slå upp information om alla datornamn och exportera till CSV-fil
+$computernames | Get-ADComputer -Properties $Properties | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath $CSVResultFile
