0d5c5af70e6ab0dc1c29591e1e3942aca69bb72f
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-19 16:13:15 +0200

    Example for Out-GridView

---
+++ Examples/SingleSubjectExamples/Show-OutGridView.ps1
@@ -0,0 +1,6 @@
+﻿<#
+Skriptet visar hur Out-GridView fungerar genom att skicka alla lokala användare till Out-GridView
+Namnen för de användare som markeras listas när man klickar OK nere till höger i listrutan.
+#>
+
+Get-LocalUser | Out-GridView -PassThru | Select-Object -ExpandProperty name
