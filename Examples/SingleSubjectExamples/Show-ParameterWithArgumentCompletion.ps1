fc8d97821af111f08c75aad83e5d1ee7300e7dee
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-19 15:45:32 +0200

    Example scripts demonstrating Tab completion

---
+++ Examples/SingleSubjectExamples/Show-ParameterWithArgumentCompletion.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Skriptet demonstrerar ArgumentCompletion som ger en parameter Tab completion
+ArgumentCompletion styr inte vilka värden som kan matas in.
+Denna funktion finns endast från och med PowerShell 6.0
+#>
+
+param (
+        [ArgumentCompletion('one','two')]
+        $Number
+    )
+
+Write-Host "ArgumentCompletion`: The number is $Number"
