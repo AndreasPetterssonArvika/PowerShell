cabd685291f493b5f371425d9b4add92adabc440
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-20 12:00:56 +0200

    Added DefaultFileName and reformat

--- Examples/SingleSubjectExamples/FilePickerDialog.ps1
+++ Examples/SingleSubjectExamples/FilePickerDialog.ps1
@@ -1,11 +1,13 @@
 ﻿# Dialogruta för att välja fil
 
-Function Get-FileName($initialDirectory)
-{  
+Function Get-FileName {
+    param (
+        [string]$InitialDirectory
+    )
     [System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”) | Out-Null
 
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.initialDirectory = $InitialDirectory
     $OpenFileDialog.filter = “Textfiler (*.txt)| *.txt”
     $OpenFileDialog.Title = "Välj fil"
     $OpenFileDialog.ShowDialog() | Out-Null
@@ -13,14 +15,18 @@ Function Get-FileName($initialDirectory)
 }
 
 # Dialogruta för att välja filnamn för att spara
-Function Get-SaveFileName($initialDirectory)
-{  
+Function Get-SaveFileName {  
+    param (
+        [string]$InitialDirectory,
+        [string]$DefaultFileName
+    )
     [System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”) | Out-Null
 
     $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.initialDirectory = $InitialDirectory
     $OpenFileDialog.filter = “Textfiler (*.txt)| *.txt”
     $OpenFileDialog.Title = "Välj fil"
+    $OpenFileDialog.filename = $DefaultFileName
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
 }
