edb9cdc33d084c52e5f035219bfc45d8c28e81c1
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-26 10:38:34 +0200

    First skeleton function

---
+++ Scripts for Usecases/VUX/Update-VUXElever.ps1
@@ -0,0 +1,36 @@
+<#
+Skript för VUX-elever
+
+#>
+[cmdletbinding()]
+param (
+    [string][Parameter(Mandatory)]$ImportFile,
+    [string]$ImportDelim
+)
+
+
+
+Write-Verbose "Startar updatering av VUX-elever"
+
+# Importera elever från fil och skapa en dictionary
+# TODO Filtrera elever redan här?
+$PCStudents = Import-Csv -Path $ImportFile -Delimiter $ImportDelim
+
+# Hämta elever från Active Directory och skapa en dictionary
+$ldapfilter = '()'
+$attributes = @()
+$ADStudents = Get-ADUser -LDAPFilter $ldapfilter -Properties $attributes
+
+# Hitta elever utan matchning som har samordningsnummer
+# Går det att hitta förslag på matchning?
+
+# Uppdatera elever som bytt samordningsnummer mot personnummer
+
+# Jämför dictionaries och skapa nya 
+# Hitta konton från Active Directory som saknar matchning i ProCapita
+# Hitta elever i ProCapita som saknar konton.
+
+# Lås gamla konton, flytta till lås-OU
+
+# Skapa nya konton med mapp
+# Generera om möjligt
