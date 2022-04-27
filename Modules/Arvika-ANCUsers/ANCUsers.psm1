26df27c831ee7a0b07365a5acce29f98d78d831e
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-04-27 17:05:02 +0200

    First files for ANC

--- Modules/Arvika-ANCUsers/ANCUsers.psm1
+++ Modules/Arvika-ANCUsers/ANCUsers.psm1
@@ -1,3 +1,42 @@
 <#
 Modul för att hantera ANC-användare
 #>
+
+function Update-ANCVUXElever {
+    [cmdletbinding()]
+    param (
+    [string][Parameter(Mandatory)]$ImportFile,
+    [string]$ImportDelim
+)
+
+    Write-Verbose "Startar updatering av VUX-elever"
+
+    # Importera elever från fil och skapa en dictionary
+    # TODO Filtrera elever redan här?
+    Write-Verbose "Path: $ImportFile"
+    Write-Verbose "Delimiter: $ImportDelimiter"
+    $PCStudents = Import-Csv -Path $ImportFile -Delimiter $ImportDelim -header identifier,Name
+    $PCStudents
+
+    # Hämta elever från Active Directory och skapa en dictionary
+    <#$ldapfilter = '()'
+    $attributes = @('mail')
+    $ADStudents = Get-ADUser -LDAPFilter $ldapfilter -Properties $attributes
+    #>
+
+    # Hitta elever utan matchning som har samordningsnummer
+    # Går det att hitta förslag på matchning?
+
+    # Uppdatera elever som bytt samordningsnummer mot personnummer
+
+    # Jämför dictionaries och skapa nya 
+    # Hitta konton från Active Directory som saknar matchning i ProCapita
+    # Hitta elever i ProCapita som saknar konton.
+
+    # Lås gamla konton, flytta till lås-OU
+
+    # Skapa nya konton med mapp
+    # Generera om möjligt de kopplade Worddokumenten för användarna
+}
+
+Export-ModuleMember -Function Update-ANCVUXElever
---
+++ Scripts for Usecases/VUX/ANCLabSetup.ps1
@@ -0,0 +1,4 @@
+$ANCVUXEleverOUName = 'VUXElever'
+$ANCVUXEleverOUPath = 'OU=Users,OU=Test,DC=test,DC=local'
+
+New-ADOrganizationalUnit -Name $ANCVUXEleverOUName -Path $ANCVUXEleverOUPath
