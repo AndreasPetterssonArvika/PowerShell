a2c3581fd0c3af9933698a27e17f708041041e4e
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-12-06 14:00:15 +0100

    Added code to read data from worksheet

--- Scripts for Usecases/XS-groups/Update-XSGroups.ps1
+++ Scripts for Usecases/XS-groups/Update-XSGroups.ps1
@@ -12,6 +12,7 @@ Import-Module ImportExcel
 
 $rektorPattern = '^Rektor [\w]{2,3}$'
 $budgetPattern = '^Budget [\w]{2,3}$'
+$identifierPattern = '^[\d]{8}-[\d]{4}$'
 
 # Slå upp alla Excelfiler i mappen
 
@@ -22,19 +23,32 @@ $worksheets = Get-ChildItem -Path $BaseFolder | Get-ExcelFileSummary | Select-Ob
 foreach ( $sheet in $worksheets ) {
     $curWSName = $sheet.Worksheetname
     if ( $curWSName -match $rektorPattern ) {
-        Write-Host "Rektor\: $curWSName"
+        Write-Verbose 'Rektorsbladet'
+        Write-Host "Rektor`: $curWSName"
     } elseif ( $curWSName -match $budgetPattern ) {
         # Gör inget
+        Write-Verbose 'Budgetbladet'
     } else {
         $curWBName = $sheet.Excelfile
         $curWBPath = $sheet.Path
         $curFile = "$curWBPath\$curWBName"
+        Write-Verbose "Enhetsblad`:$curWSName"
         #$curWBPath
-        $curContent = Import-Excel -Path $curFile -WorksheetName $curWSName -NoHeader -StartColumn 2 -EndColumn 4
+        $curContent = Import-Excel -Path $curFile -WorksheetName $curWSName -NoHeader -StartColumn 2 -EndColumn 6
         
+        #<#
         foreach ( $row in $curContent ) {
-            
+            $curDept = $row.P1
+            $curID = $row.P2
+            $curTitleAbbr = $row.P5
+            #<#
+            if ( $curID -match $identifierPattern ) {
+                # Matchar identifierare
+                Write-Verbose "Hittade data`: $curDept, $curID, $curTitleAbbr"
+            }
+            #>
         }
+        #>
     }
 }
 #>
