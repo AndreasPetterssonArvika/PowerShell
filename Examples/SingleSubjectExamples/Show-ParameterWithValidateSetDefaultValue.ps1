c74a78329fefc9047b722b9918810e10b06c96a5
Andreas Pettersson <andreas.pettersson@arvika.se>
2022-12-08 13:51:25 +0100

    Added example default value ValidateSet

---
+++ Examples/SingleSubjectExamples/Show-ParameterWithValidateSetDefaultValue.ps1
@@ -0,0 +1,11 @@
+﻿<#
+Skriptet demonstrerar ValidateSet som ger en parameter Tab completion
+ValidateSet styr så att enbart värden från listan kan användas
+#>
+
+param (
+        [validateSet('one','two')]
+        [string[]]$Number = ('two')
+    )
+
+Write-Host "ValidateSet`: The number is $Number"
