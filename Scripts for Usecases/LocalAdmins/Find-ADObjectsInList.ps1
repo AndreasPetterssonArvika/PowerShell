0066f1d68c78679cf95ce30d68f00c61027429d4
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-03-30 08:27:15 +0200

    output to pipeline and cleanup

--- Scripts for Usecases/LocalAdmins/Find-ADObjectsInList.ps1
+++ Scripts for Usecases/LocalAdmins/Find-ADObjectsInList.ps1
@@ -15,20 +15,27 @@ $FileHeader = 'AssetName'
 $OU='OU=KLS Biblioteket Datorer,OU=KLS Biblioteket,OU=KLS Kultur och fritid,OU=KLS Kommunledningsstab,DC=arvika,DC=se'
 #>
 
+$missing = 'missing'
+$found = 'found'
+
 [hashtable]$ImportedNames = @{}
 
 # Hämta alla unika namn från indatafilen till en hashtable
 Import-Csv -Path $InputFile -Delimiter ';' | select-object -ExpandProperty $FileHeader | ForEach-Object { $ImportedNames[$_]='ADObject' }
 
-#$ImportedNames.Keys
-
 
 # Slå upp alla datorer ur OU och kolla mot hashtable med namn
 $objNotInList = @{}
-#Get-ADObject -Filter * -SearchBase $OU -SearchScope Subtree
-Get-ADObject -Filter * -SearchBase $OU -SearchScope Subtree -Properties cn | Select-Object -ExpandProperty cn | ForEach-Object { if ( $ImportedNames.ContainsKey($_) ) { $objNotInList[$_]='found' } else { $objNotInList[$_]='missing' } }
 
+Get-ADObject -Filter * -SearchBase $OU -SearchScope Subtree -Properties cn | Select-Object -ExpandProperty cn | ForEach-Object { if ( $ImportedNames.ContainsKey($_) ) { $objNotInList[$_]=$found } else { $objNotInList[$_]=$missing } }
+
+# Hitta de som inte finns i listan och skriv dem till pipeline, Write-Output
 foreach ( $key in $objNotInList.Keys ) {
     $status = $objNotInList[$key]
-    Write-output "$key`: $status"
+    Write-Debug "$key`: $status"
+    if ( $status -eq $missing ) {
+        Write-Output $key
+    }
+
+    
 }
