8d4f24869f1e7dd351d6032f743b59803a25242b
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-06-07 10:40:10 +0200

    Debug, correct ouput path

--- Utility/Compress-Subfolders.ps1
+++ Utility/Compress-Subfolders.ps1
@@ -1,10 +1,12 @@
 <#
 Skriptet tar en mapp som parameter och komprimerar alla undermappar till enskilda zip-filer
+Zip-filerna får samma namn som mappen som komprimeras och har filändelsen .zip
 #>
 
 [cmdletbinding()]
 param(
-    [string][Parameter(Mandatory)]$BaseFolder
+    [string][Parameter(Mandatory)]$BaseFolder,
+    [string][parameter(Mandatory)]$DestinationFolder
 )
  
 # Hämta alla undermappar
@@ -13,7 +15,8 @@ foreach ($s in $subfolders) {
  
 $folderpath = $s.FullName
 $foldername = $s.Name
+$CompFileName = "$foldername.zip"
  
-Compress-Archive -Path $folderpath -DestinationPath $BaseFolder\$foldername
+Compress-Archive -Path $folderpath -DestinationPath $DestinationFolder\$CompFileName
  
 }
