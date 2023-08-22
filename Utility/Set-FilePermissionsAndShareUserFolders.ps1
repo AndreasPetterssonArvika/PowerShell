6970c185992bd25a3f285bcb7e262eedf907f901
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-08-22 11:19:01 +0200

    Changed script to handle pipelining

--- Utility/Set-FilePermissionsAndShareUserFolders.ps1
+++ Utility/Set-FilePermissionsAndShareUserFolders.ps1
@@ -17,7 +17,7 @@ Skriptet måste köras på samma maskin där mapparna ligger
 
 [cmdletbinding()]
 param (
-    [Parameter(Mandatory)][string]$parentFolderPath,     # Sökvägen till den överordnade mappen
+    [Parameter(Mandatory,ValueFromPipeline)][string]$parentFolderPath,     # Sökvägen till den överordnade mappen
     [Parameter()][string]$outputFilePath                 # Sökvägen till textfilen där mappar utan användarnamn ska sparas. Valfri, sparar annars i skriptmappen
 )
 
@@ -27,52 +27,63 @@ param (
 # Ange sökvägen till textfilen där mappar utan användarnamn ska sparas
 #$outputFilePath = "C:\Path\Till\Output\Resultat.txt"
 
-# Hantera fallet där en utdatafil inte meddelats via parameter
-if ( -not $outputFilePath ) {
-    Write-Verbose "Ingen sökväg angiven för utdatafil för mappar som inte har motsvarande konto"
-    # Kontrollera om objektet $psISE finns
-    if ($psISE) {
-        # Objektet finns, skriptet körs från ISE.
-        # Hämta sökvägen från $psISE
-        $basePath = Split-Path -Path $psISE.CurrentFile.FullPath
-    } else {
-        # Alla andra fall, använd $PSScriptRoot
-        $basePath = $PSScriptRoot
+begin {
+    # Hantera fallet där en utdatafil inte meddelats via parameter
+    if ( -not $outputFilePath ) {
+        Write-Verbose "Ingen sökväg angiven för utdatafil för mappar som inte har motsvarande konto"
+        # Kontrollera om objektet $psISE finns
+        if ($psISE) {
+            # Objektet finns, skriptet körs från ISE.
+            # Hämta sökvägen från $psISE
+            $basePath = Split-Path -Path $psISE.CurrentFile.FullPath
+        } else {
+            # Alla andra fall, använd $PSScriptRoot
+            $basePath = $PSScriptRoot
+        }
+    
+        $now = Get-Date -Format 'yyMMdd_HHmm'
+    
+        $outputFilePath = "$basePath\FoldersWithoutUsers_$now.txt"
+        
     }
 
-    $now = Get-Date -Format 'yyMMdd_HHmm'
+    Write-verbose "Loggar mappar utan motsvarande användare till filen $outputFilePath"
 
-    $outputFilePath = "$basePath\FoldersWithoutUsers_$now.txt"
-    
 }
 
-Write-verbose "Loggar mappar utan motsvarande användare till filen $outputFilePath"
+process {
 
-# Hämta en lista över alla undermappar i den överordnade mappen
-$subFolders = Get-ChildItem -Path $parentFolderPath -Directory
+    # Hämta en lista över alla undermappar i den överordnade mappen
+    $subFolders = Get-ChildItem -Path $parentFolderPath -Directory
 
-# Loopa igenom varje undermapp
-foreach ($subFolder in $subFolders) {
-    # Kontrollera om undermappens namn matchar ett sAMAccountName i Active Directory
-    $username = $subFolder.Name
-    $user = Get-ADUser -Filter { SamAccountName -eq $username } -ErrorAction SilentlyContinue
-    
-    if ($user) {
-        # Ge användaren full behörighet till mappen
-        $acl = Get-Acl -Path $subFolder.FullName
-        $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($user.SamAccountName, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
-        $acl.AddAccessRule($rule)
-        Set-Acl -Path $subFolder.FullName -AclObject $acl
+    # Loopa igenom varje undermapp
+    foreach ($subFolder in $subFolders) {
+        # Kontrollera om undermappens namn matchar ett sAMAccountName i Active Directory
+        $username = $subFolder.Name
+        $user = Get-ADUser -Filter { SamAccountName -eq $username } -ErrorAction SilentlyContinue
         
-        # Skapa en dold share för mappen
-        $shareName = $user.SamAccountName + "$"
-        New-SmbShare -Name $shareName -Path $subFolder.FullName -CachingMode None -EncryptData $false -FullAccess Everyone
-        
-        Write-Verbose "Behörigheter och delning skapade för användare $($user.SamAccountName) i mappen $($subFolder.FullName)"
-    } else {
-        # Skriv mappens sökväg till textfilen om det inte finns ett matchande användarnamn
-        Add-Content -Path $outputFilePath -Value $subFolder.FullName
-        Write-Verbose "Inget matchande användarnamn funnet för mappen $($subFolder.FullName)"
-        Write-Verbose "Loggar till utdatafilen."
+        if ($user) {
+            # Ge användaren full behörighet till mappen
+            $acl = Get-Acl -Path $subFolder.FullName
+            $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($user.SamAccountName, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+            $acl.AddAccessRule($rule)
+            Set-Acl -Path $subFolder.FullName -AclObject $acl
+            
+            # Skapa en dold share för mappen
+            $shareName = $user.SamAccountName + "$"
+            New-SmbShare -Name $shareName -Path $subFolder.FullName -CachingMode None -EncryptData $false -FullAccess Everyone | Out-Null
+            
+            Write-Verbose "Behörigheter och delning skapade för användare $($user.SamAccountName) i mappen $($subFolder.FullName)"
+        } else {
+            # Skriv mappens sökväg till textfilen om det inte finns ett matchande användarnamn
+            Add-Content -Path $outputFilePath -Value $subFolder.FullName
+            Write-Verbose "Inget matchande användarnamn funnet för mappen $($subFolder.FullName)"
+            Write-Verbose "Loggar till utdatafilen."
+        }
     }
+
+    end {}
+
 }
+
+
