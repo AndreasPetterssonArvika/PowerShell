d4a1e595776a5190f636d56bf832b39692ddad50
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-08-30 13:19:54 +0200

    Cleaned upp comments in example

--- Examples/SingleSubjectExamples/HashtableComparison.ps1
+++ Examples/SingleSubjectExamples/HashtableComparison.ps1
@@ -4,7 +4,10 @@ Jämför hashtables
 
 function Compare-HashtableKeys {
     <#
-    Funktionen jÃ¤mfÃ¶r hashtables
+    Funktionen jämför hashtables
+    Data innehåller det data man är intresserad av, alla värden som returneras finns i denna hashtable.
+    Comp innehåller det man ska använda för jämförelse.
+    CommonKeys anger att unionen av hashtables ska returneras, allså alla i Data som också finns i Comp.
     #>
 
     [cmdletbinding()]
@@ -19,18 +22,18 @@ function Compare-HashtableKeys {
     if ( $CommonKeys ) {
         foreach ( $key in $Data.Keys ) {
             if ( $Comp.ContainsKey( $key ) ) {
-                # Gemensamma vÃ¤rden ska returneras, lÃ¤gg till i returen
+                # Gemensamma värden ska returneras, lägg till i returen
                 $return[$key]='common'
             } else {
-                # GÃ¶r inget
+                # Gör inget
             }
         }
     } else {
         foreach ( $key in $Data.Keys ) {
             if ( $Comp.ContainsKey( $key ) ) {
-                # Diffen ska returneras, gÃ¶r inget
+                # Diffen ska returneras, gör inget
             } else {
-                # Skilda vÃ¤rden ska returneras, lÃ¤gg till i returen
+                # Skilda värden ska returneras, lägg till i returen
                 $return[$key]='diff'
             }
         }
