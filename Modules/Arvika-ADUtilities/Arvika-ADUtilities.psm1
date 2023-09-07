12278d7c43b8fc6d3737981dfcf2c69ac31955db
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-09-07 08:01:09 +0200

    Changed selection of data

--- Modules/Arvika-ADUtilities/Arvika-ADUtilities.psm1
+++ Modules/Arvika-ADUtilities/Arvika-ADUtilities.psm1
@@ -349,6 +349,7 @@ function Get-ImmutableIDForUser {
 Funktionen tar emot en ADUser från pipeline och lämnar specifika data som en semikolonseparerad textsträng
 Ärende #45799
 Issue #240
+Issue #243, ändrade data i exporten
 #>
 function Get-SVUserData {
     [cmdletbinding()]
@@ -359,11 +360,15 @@ function Get-SVUserData {
     begin {}
 
     process {
-        $curUser = $ADUser | Get-ADUser -Properties displayName,userPrincipalName,mail
-        $userdata = $curuser.displayName + ";" + $curUser.userPrincipalName + ";"  + $curUser.mail
-        $ImmutableID = Get-ImmutableIDForUser -ADUser $ADUser
-        $userdata += ";" + $ImmutableID
-        Write-Output $userdata
+        $curUser = $ADUser | Get-ADUser -Properties givenName,SN,mail
+        $curMail = $curUser.mail
+        if ( $curMail ) {
+            $userdata = $curuser.givenName + ";" + $curUser.SN + ";"  + $curUser.mail
+            $ImmutableID = Get-ImmutableIDForUser -ADUser $ADUser
+            $userdata += ";" + $ImmutableID
+            Write-Output $userdata
+        }
+        
     }
 
     end {}
