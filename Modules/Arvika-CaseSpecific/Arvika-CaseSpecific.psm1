d27d9ad42e2eed9e3ec66eb9df8ea5c8c9dd245e
Andreas Pettersson <andreas.pettersson@arvika.se>
2023-11-03 11:45:03 +0100

    Function to get users by time finished

--- Modules/Arvika-CaseSpecific/Arvika-CaseSpecific.psm1
+++ Modules/Arvika-CaseSpecific/Arvika-CaseSpecific.psm1
@@ -31,4 +31,43 @@ function Get-SVUserData {
     end {}
 }
 
-Export-ModuleMember Get-SVUserData
+<#
+Funktioner för att hantera de nya skolanknutna kontona på arvika.se
+Ärende #47690
+Issue #255
+#>
+
+<#
+Funktionen skapar en CSV-fil med de aktuella användarna.
+
+Datumen skapas t ex genom ((Get-Date).AddDays(<days>)).Date
+VIKTIGT: Ovanstående kommando fungerar
+((Get-Date).AddDays(<days>)).DateTime fungerar inte. Det kommandot lämnar ifrån sig en
+textsträng och inte ett DateTime-objekt.
+
+Attribut som behövs
+- personNummer
+- mail
+#>
+function Get-MIMStartNewUsers {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory)][DateTime]$FromTime,
+        [Parameter(Mandatory)][DateTime]$ToTime,
+        [Parameter(Mandatory)][string]$UserIdentifier,
+        [Parameter()][string]$OutputFolder='.'
+    )
+
+    $now = Get-Date -Format 'yyyyMMdd_HHmm'
+
+    $outfile = "$OutputFolder\NewMIMUsers_$now.csv"
+
+    "$UserIdentifier,mail" | Out-File -FilePath $outfile -Encoding utf8
+
+    $attrs = @("$UserIdentifier",'mail')
+
+    Get-ADUser -Filter { (whenCreated -ge $FromTime) -and (whenCreated -le $ToTime) } -Properties $attrs | ForEach-Object { $curUserRow="$($_.$UserIdentifier);$($_.mail)";$curUserRow | Out-File -FilePath $outfile -Encoding utf8 -Append }
+}
+
+Export-ModuleMember -Function Get-SVUserData
+Export-ModuleMember -Function Get-MIMStartNewUsers
